0cc93373da2332f4a818cbbe6a7b79030da6c283
Blackswordsman <74133903+A1ncrad@users.noreply.github.com>
2024-02-02 17:10:00 +0100

    hotfix

--- setup.bat
+++ setup.bat
@@ -14,37 +14,37 @@ start cmd /k firefox
 
 
 winget install KeePassXCTeam.KeePassXC
-$keepass = 'C:\Program Files\KeePassXC';
+$keepass = 'C:\Program Files\KeePassXC'
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";" + $keepass, "Machine")
 $env:path = $env:path + ";" + $keepass;
 keepassxc
 
 
 winget install Telegram.TelegramDesktop 
-$telegram = 'C:\Users\timur\AppData\Roaming\Telegram Desktop';
+$telegram = 'C:\Users\timur\AppData\Roaming\Telegram Desktop'
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";" + $telegram, "Machine")
-$env:path = $env:path + ";" + $telegram;
+$env:path = $env:path + ";" + $telegram
 telegram
 
 
 winget install Valve.Steam 
-$steam = 'C:\Program Files (x86)\Steam';
+$steam = 'C:\Program Files (x86)\Steam'
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";" + $steam, "Machine")
-$env:path = $env:path + ";" + $steam;
+$env:path = $env:path + ";" + $steam
 steam
 
 
 winget install Discord.Discord
 $discord = 'C:\Users\timur\AppData\Local\Discord'
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";" + $discord, "Machine")
-$env:path = $env:path + ";" + $discord;
+$env:path = $env:path + ";" + $discord
 discord
 
 
 
-$hexified = "00,00,00,00,00,00,00,00,02,00,00,00,1d,00,3a,00,00,00,00,00".Split(',') | % { "0x$_"};
-$kbLayout = 'HKLM:\System\CurrentControlSet\Control\Keyboard Layout';
-New-ItemProperty -Path $kbLayout -Name "Scancode Map" -PropertyType Binary -Value ([byte[]]$hexified);
+$hexified = "00,00,00,00,00,00,00,00,02,00,00,00,1d,00,3a,00,00,00,00,00".Split(',') | % { "0x$_"}
+$kbLayout = 'HKLM:\System\CurrentControlSet\Control\Keyboard Layout'
+New-ItemProperty -Path $kbLayout -Name "Scancode Map" -PropertyType Binary -Value ([byte[]]$hexified)
 
 
 wsl --install
